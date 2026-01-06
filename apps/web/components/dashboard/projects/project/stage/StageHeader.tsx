f429d87514c46547f3c6d93a8ee450fd32b5c20f
ahmed kotwal <ahmedkotwal03@gmail.com>
2026-01-06 21:01:15 +0000

    htdtdgdg

--- apps/web/components/dashboard/projects/project/stage/StageHeader.tsx
+++ apps/web/components/dashboard/projects/project/stage/StageHeader.tsx
@@ -1,19 +1,27 @@
 import { useStageContext } from '@contexts/StageContext.tsx';
 import { stageTabs } from '@projective/types';
+import { useProjectContext } from '@contexts/ProjectContext.tsx';
+import { useEffect } from 'preact/hooks';
+import { IconDotsVertical } from '@tabler/icons-preact';
 
 export default function StageHeader() {
 	let tabs: { label: string; href: string }[] = [];
-	const { stage } = useStageContext();
+	const { stage, stage_id } = useStageContext();
+	const { project_id } = useProjectContext();
 
 	if (stage.value) {
 		tabs = stageTabs(stage.value.stage_type);
 	}
 
+	useEffect(() => {
+console.log(globalThis.location.href)
+	}, [])
+
 	return (
 		<div className='stage-header'>
 			<div className='stage-header__left'>
 				<img
-					src={'https://placehold.co/50'}
+					src="https://placehold.co/50"
 					className='sidebar-details__avatar'
 				/>
 				<div>
@@ -23,10 +31,16 @@ export default function StageHeader() {
 			</div>
 			<div className='stage-header__center'>
 				{tabs.map((tab) => (
-					<a key={tab.href} className='stage-header__tab' href={tab.href}>{tab.label}</a>
+					<a key={tab.href} 
+					className='stage-header__tab'
+					data-current={globalThis.location.href.includes(`/projects/${project_id.value}/${stage_id.value}/${tab.href}`)} 
+					href={`/projects/${project_id.value}/${stage_id.value}/${tab.href}`} 
+					f-partial={`/projects/${project_id.value}/${stage_id.value}/${tab.href}`}>{tab.label}</a>
 				))}
 			</div>
-			<div className='stage-header__right'></div>
+			<div className='stage-header__right'>
+				<IconDotsVertical/>
+			</div>
 		</div>
 	);
 }
--- apps/web/routes/(dashboard)/projects/[projectid]/[stageid]/chat.tsx
+++ apps/web/routes/(dashboard)/projects/[projectid]/[stageid]/chat.tsx
@@ -1,7 +1,6 @@
 export default function Projects() {
 	return (
 		<div>
-			<h1>Projects Chat Page</h1>
 		</div>
 	);
 }
